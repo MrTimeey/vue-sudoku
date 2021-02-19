aa417bff03bb091a4047ac8b7859fd67ae631c77
Tim Siegler <coding@tim-siegler.com>
2021-02-19 14:42:42 +0100

    - Init router and store

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 import vuetify from './plugins/vuetify';
+import store from './store'
 import ForkeMeOnGithub from 'fork-me-on-github-vue';
 
 Vue.use(ForkeMeOnGithub);
@@ -8,6 +10,8 @@ Vue.use(ForkeMeOnGithub);
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   vuetify,
+  store,
   render: h => h(App),
 }).$mount('#app')
