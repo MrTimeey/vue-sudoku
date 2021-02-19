86bbdc024fab2e6f4f524eaf243581fe416bdffa
Tim Siegler <coding@tim-siegler.com>
2021-02-19 14:34:11 +0100

    Add vuetify

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
 import ForkeMeOnGithub from 'fork-me-on-github-vue';
 
 Vue.use(ForkeMeOnGithub);
@@ -7,5 +8,6 @@ Vue.use(ForkeMeOnGithub);
 Vue.config.productionTip = false
 
 new Vue({
+  vuetify,
   render: h => h(App),
 }).$mount('#app')
