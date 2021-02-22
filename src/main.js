5753173a4eebcefe089fa701d24db69ed6871e57
Tim Siegler <coding@tim-siegler.com>
2021-02-22 13:15:43 +0100

    Add i18n support

--- src/main.js
+++ src/main.js
@@ -4,12 +4,16 @@ import router from './router'
 import vuetify from './plugins/vuetify';
 import store from './store'
 import ForkeMeOnGithub from 'fork-me-on-github-vue';
+import i18n from '@/plugins/i18n';
+import VueI18n from "vue-i18n";
 
 Vue.use(ForkeMeOnGithub);
+Vue.use(VueI18n);
 
 Vue.config.productionTip = false
 
 new Vue({
+  i18n,
   router,
   vuetify,
   store,
