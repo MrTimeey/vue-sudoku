64af19d1f3ad2ce006df0611ab33a5565382fbc3
Tim Siegler <coding@tim-siegler.com>
2021-02-22 14:10:07 +0100

    Revert "Add i18n support to router"
    This reverts commit 24042886

--- src/router/index.js
+++ src/router/index.js
@@ -5,52 +5,37 @@ import NotFound from "@/views/NotFound";
 import StartPage from "@/views/StartPage";
 import GamePage from "@/views/GamePage";
 import ResultPage from "@/views/ResultPage";
-import i18n from '../plugins/i18n'
-
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    redirect: `/${i18n.locale}`,
+    redirect: '/startPage',
     name: 'blank',
+    component: StartPage
   },
   {
-    path: '/:lang',
-    component: {
-      render (c) { return c('router-view') }
-    },
-    children: [
-      {
-        path: '/',
-        name: 'home',
-        redirect: `/${i18n.locale}/startPage`,
-      },
-      {
-        path: 'startPage',
-        name: 'StartPage',
-        component: StartPage
-      },
-      {
-        path: 'game',
-        name: 'GamePage',
-        component: GamePage,
-        beforeEnter: requireGameStarted
-      },
-      {
-        path: '/game',
-        name: 'ResultPage',
-        component: ResultPage,
-        beforeEnter: requireGameStarted
-      },
-      {
-        path: "*",
-        component: NotFound
-      }
-    ]
+    path: '/startPage',
+    name: 'StartPage',
+    component: StartPage
   },
-
+  {
+    path: '/game',
+    name: 'GamePage',
+    component: GamePage,
+    beforeEnter: requireGameStarted
+  },
+  {
+    path: '/game',
+    name: 'ResultPage',
+    component: ResultPage,
+    beforeEnter: requireGameStarted
+  },
+  {
+    path: "*",
+    component: NotFound
+  }
 ]
 
 const router = new VueRouter({
